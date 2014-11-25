fa7e21f894202b2270297b0461735d511727a184
Jimmy <jimmylorunning@gmail.com>
2014-11-25 16:19:20 -0500

    hw part 1 refactored with th_class() method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
+
 	def header_sort_link(link_text, sort_param)
 	 link_to(link_text, movies_path({sort: sort_param}), id: "#{sort_param}_header")
 	end
+
+	def th_class(hilite)
+		return {class: 'hilite'} if hilite
+		return {}
+	end
+
 end
